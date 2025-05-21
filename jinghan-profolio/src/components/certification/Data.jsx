9152d293501d052024031601b70f1228f501dbcd
hann2613 <hann2626@163.com>
2025-05-21 15:16:56 +0100

    1

--- jinghan-profolio/src/components/certification/Data.jsx
+++ jinghan-profolio/src/components/certification/Data.jsx
@@ -8,36 +8,35 @@ export const Data = [
   {
     id: 1,
     image: Image1,
-    title: "Responsive Web Design",
-    description:
-      "Completed several web development projects using HTML, CSS, and JavaScript. These projects demonstrate my ability to create responsive, interactive, and visually appealing web pages.",
-  },
-  {
-    id: 2,
-    image: Image2,
-    title: "Introduction to Data & SQL certificate",
-    description:
-      "In addition to my master's coursework, I participated in and completed an 8-week Data & SQL course organized by CFG, where I strengthened my data analysis, SQL, and ERD skills, and successfully designed and analyzed a library database project.",
-  },
-  {
-    id: 3,
-    image: Image3,
     title: "AWS Certified Cloud Practitioner",
     description:
       "Validates core AWS cloud knowledge, including architecture, services, and cost optimization, essential for scalable IT and data infrastructure management.",
   },
   {
-    id: 4,
-    image: Image4,
+    id: 2,
+    image: Image2,
     title: "Oracle Infrastructure 2025 Certified AI Foundation Associate",
     description:
       "Demonstrates foundational AI expertise in Oracle Cloud Infrastructure (OCI), including AI concepts, tools, and practical applications for data-driven solutions.",
   },
   {
-    id: 5,
-    image: Image5,
+    id: 3,
+    image: Image3,
     title: "HubSpot SEO Certified",
     description:
       "Covers search engine optimization strategies, enabling data-backed website optimization to enhance visibility and support digital product growth.",
+  },{
+    id: 4,
+    image: Image4,
+    title: "Responsive Web Design",
+    description:
+      "Completed several web development projects using HTML, CSS, and JavaScript. These projects demonstrate my ability to create responsive, interactive, and visually appealing web pages.",
   },
+  {
+    id: 5,
+    image: Image5,
+    title: "Introduction to Data & SQL certificate",
+    description:
+      "In addition to my master's coursework, I participated in and completed an 8-week Data & SQL course organized by CFG, where I strengthened my data analysis, SQL, and ERD skills, and successfully designed and analyzed a library database project.",
+  }
 ];
