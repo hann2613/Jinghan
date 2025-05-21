9c00a302bfd54c24285fe71ba060b85e6e603bd8
hann2613 <hann2626@163.com>
2025-05-21 13:43:02 +0100

    all

--- jinghan-profolio/src/components/certification/Data.jsx
+++ jinghan-profolio/src/components/certification/Data.jsx
@@ -1,5 +1,8 @@
 import Image1 from "../../assets/certification1.png";
 import Image2 from "../../assets/certification2.png";
+import Image3 from "../../assets/certification3.png";
+import Image4 from "../../assets/certification4.png";
+import Image5 from "../../assets/certification5.png";
 
 export const Data = [
   {
@@ -15,5 +18,26 @@ export const Data = [
     title: "Introduction to Data & SQL certificate",
     description:
       "In addition to my master's coursework, I participated in and completed an 8-week Data & SQL course organized by CFG, where I strengthened my data analysis, SQL, and ERD skills, and successfully designed and analyzed a library database project.",
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "AWS Certified Cloud Practitioner",
+    description:
+      "Validates core AWS cloud knowledge, including architecture, services, and cost optimization, essential for scalable IT and data infrastructure management.",
+  },
+  {
+    id: 4,
+    image: Image4,
+    title: "Oracle Infrastructure 2025 Certified AI Foundation Associate",
+    description:
+      "Demonstrates foundational AI expertise in Oracle Cloud Infrastructure (OCI), including AI concepts, tools, and practical applications for data-driven solutions.",
+  },
+  {
+    id: 5,
+    image: Image5,
+    title: "HubSpot SEO Certified",
+    description:
+      "Covers search engine optimization strategies, enabling data-backed website optimization to enhance visibility and support digital product growth.",
   },
 ];
